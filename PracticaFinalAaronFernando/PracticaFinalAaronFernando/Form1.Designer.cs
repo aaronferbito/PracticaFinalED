eb3712fd6adda2bab68ec85f9cf996754664dd45
Ferbito <fmore.g@gmail.com>
2019-05-27 13:07:13 +0200

    Menu. Completo

--- PracticaFinalAaronFernando/PracticaFinalAaronFernando/Form1.Designer.cs
+++ PracticaFinalAaronFernando/PracticaFinalAaronFernando/Form1.Designer.cs
@@ -29,15 +29,107 @@
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cOMPRASToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vENTASToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sTOCKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fILTROToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.cOMPRASToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.vENTASToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.sTCOKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eSTADISTICASToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.cOMPRASToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.vENTASToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.sTCOKToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cOMPRASToolStripMenuItem,
+            this.vENTASToolStripMenuItem,
+            this.sTOCKToolStripMenuItem,
+            this.fILTROToolStripMenuItem1,
+            this.eSTADISTICASToolStripMenuItem1});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(973, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cOMPRASToolStripMenuItem
+            // 
+            this.cOMPRASToolStripMenuItem.Name = "cOMPRASToolStripMenuItem";
+            this.cOMPRASToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.cOMPRASToolStripMenuItem.Text = "COMPRAS";
+            // 
+            // vENTASToolStripMenuItem
+            // 
+            this.vENTASToolStripMenuItem.Name = "vENTASToolStripMenuItem";
+            this.vENTASToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.vENTASToolStripMenuItem.Text = "VENTAS";
+            // 
+            // sTOCKToolStripMenuItem
+            // 
+            this.sTOCKToolStripMenuItem.Name = "sTOCKToolStripMenuItem";
+            this.sTOCKToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.sTOCKToolStripMenuItem.Text = "STOCK";
+            // 
+            // fILTROToolStripMenuItem1
+            // 
+            this.fILTROToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cOMPRASToolStripMenuItem1,
+            this.vENTASToolStripMenuItem1,
+            this.sTCOKToolStripMenuItem});
+            this.fILTROToolStripMenuItem1.Name = "fILTROToolStripMenuItem1";
+            this.fILTROToolStripMenuItem1.Size = new System.Drawing.Size(62, 20);
+            this.fILTROToolStripMenuItem1.Text = "FILTROS";
+            // 
+            // cOMPRASToolStripMenuItem1
+            // 
+            this.cOMPRASToolStripMenuItem1.Name = "cOMPRASToolStripMenuItem1";
+            this.cOMPRASToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.cOMPRASToolStripMenuItem1.Text = "COMPRAS";
+            // 
+            // vENTASToolStripMenuItem1
+            // 
+            this.vENTASToolStripMenuItem1.Name = "vENTASToolStripMenuItem1";
+            this.vENTASToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.vENTASToolStripMenuItem1.Text = "VENTAS";
+            // 
+            // sTCOKToolStripMenuItem
+            // 
+            this.sTCOKToolStripMenuItem.Name = "sTCOKToolStripMenuItem";
+            this.sTCOKToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sTCOKToolStripMenuItem.Text = "STCOK";
+            // 
+            // eSTADISTICASToolStripMenuItem1
+            // 
+            this.eSTADISTICASToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cOMPRASToolStripMenuItem2,
+            this.vENTASToolStripMenuItem2,
+            this.sTCOKToolStripMenuItem1});
+            this.eSTADISTICASToolStripMenuItem1.Name = "eSTADISTICASToolStripMenuItem1";
+            this.eSTADISTICASToolStripMenuItem1.Size = new System.Drawing.Size(94, 20);
+            this.eSTADISTICASToolStripMenuItem1.Text = "ESTADISTICAS";
+            // 
+            // cOMPRASToolStripMenuItem2
+            // 
+            this.cOMPRASToolStripMenuItem2.Name = "cOMPRASToolStripMenuItem2";
+            this.cOMPRASToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.cOMPRASToolStripMenuItem2.Text = "COMPRAS";
+            // 
+            // vENTASToolStripMenuItem2
+            // 
+            this.vENTASToolStripMenuItem2.Name = "vENTASToolStripMenuItem2";
+            this.vENTASToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.vENTASToolStripMenuItem2.Text = "VENTAS";
+            // 
+            // sTCOKToolStripMenuItem1
+            // 
+            this.sTCOKToolStripMenuItem1.Name = "sTCOKToolStripMenuItem1";
+            this.sTCOKToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.sTCOKToolStripMenuItem1.Text = "STCOK";
             // 
             // Form1
             // 
@@ -49,6 +141,8 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,6 +151,17 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cOMPRASToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vENTASToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sTOCKToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fILTROToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem cOMPRASToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem vENTASToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem sTCOKToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eSTADISTICASToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem cOMPRASToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem vENTASToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem sTCOKToolStripMenuItem1;
     }
 }
 
